9c2a6312155efc8379e10b19971917186eac61d9
AleksNau <workciti@yandex.ru>
2022-10-09 16:12:12 +0300

    complete

--- 01-cards-sources/app.js
+++ 01-cards-sources/app.js
@@ -1,15 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+function ActiveClass(active) {
+   const slides = document.querySelectorAll('.slide');
+   slides[active].classList.add('active');
 
-for (const slide of slides) {
-   slide.addEventListener('click', () => {
-      clearActiveClass();
+   for (const slide of slides) {
+      slide.addEventListener('click', () => {
+         clearActiveClass();
 
-      slide.classList.add('active');
-   })
+         slide.classList.add('active');
+      })
+   }
+
+   function clearActiveClass() {
+      slides.forEach((slide) => {
+         slide.classList.remove('active');
+      })
+   }
 }
 
-function clearActiveClass() {
-   slides.forEach((slide) => {
-      slide.classList.remove('active');
-   })
-}
+ActiveClass(2);
